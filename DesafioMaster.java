786db91da06ab45a467a28541bbaead915e9bbe8
fabinhodev10 <viniciusandrade121212@gmail.com>
2025-01-12 22:37:10 -0300

    DesafioMaster.java

--- DesafioMaster.java
+++ DesafioMaster.java
@@ -9,45 +9,45 @@ public class DesafioMaster {
         int menu = 0;
 
         System.out.println("************MENU CONTA***********");
-        System.out.println("Nome:                  "+nome);
+        System.out.println("\nNome:                  "+nome);
         System.out.println("Saldo atualizado       "+saldoAtual);
 
-        System.out.println("OPÇÕES" +
+        System.out.println("\nOPÇÕES" +
                 "" +
                 "" +
                 "" +
                 "");
 
         while (menu != 4){
-            System.out.println("1 - consultar saldo");
+            System.out.println("\n1 - consultar saldo");
             System.out.println("2 - Receber valor");
             System.out.println("3 - transferir valor");
             System.out.println("4 - sair");
-            System.out.println("Escolha uma opção");
+            System.out.println("\nEscolha uma opção");
             menu = leitura.nextInt();
 
             if (menu == 1){
-                System.out.println("o saldo atualizado é de "+saldoAtual);
+                System.out.println("\no saldo atualizado é de "+saldoAtual);
             } else if (menu ==2) {
                 System.out.println("qual valor deseja depositar:");
                 double recebeValor  = leitura.nextDouble();
                 saldoAtual = saldoAtual + recebeValor;
-                System.out.println("valor depositado, seu saldo atual é de "+saldoAtual);
+                System.out.println("\nvalor depositado, seu saldo atual é de "+saldoAtual);
             } else if (menu == 3) {
                 System.out.println("qual valor deseja enviar:");
                 double enviaValor  = leitura.nextDouble();
 
                 if (enviaValor > saldoAtual){
-                    System.out.println("Voce nao tem saldo suficiente :( VOCE PODE ENVIAR VALORES ABAIXO QUE "+saldoAtual);
+                    System.out.println("\nVoce nao tem saldo suficiente :( VOCE PODE ENVIAR VALORES ABAIXO QUE "+saldoAtual);
                 } else {
                     saldoAtual = saldoAtual - enviaValor;
-                    System.out.println("valor enviado, seu saldo atual é de "+saldoAtual);
+                    System.out.println("\nvalor enviado, seu saldo atual é de "+saldoAtual);
                 }
 
             } else if (menu == 4) {
                 System.out.println("saindo do sistema");
             } else {
-                System.out.println("Opção invalida, digite uma opção de 1 a 4");
+                System.out.println("\nOpção invalida, digite uma opção de 1 a 4");
             }
 
         }
